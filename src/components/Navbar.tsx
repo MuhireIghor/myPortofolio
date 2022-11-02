45f9d074f08f97e1c714073fcb685271308e37aa
MuhireIghor <muhireighor123@gmail.com>
2022-11-02 11:19:31 +0200

    Data fetching

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,14 @@ import {NavLink} from 'react-router-dom';
 export function Navbar(){
     return(
         
-<NavbarBs className='bg-white shadow-sm mb-3'>
+<NavbarBs sticky='top' className='bg-white shadow-sm mb-3'>
             <Container>
     <Nav className='me-auto'>
 <Nav.Link to='/' as={NavLink}>Home</Nav.Link>
 <Nav.Link to='/about' as={NavLink}>About</Nav.Link>
 <Nav.Link to='/store' as={NavLink}>Store</Nav.Link>
     </Nav>
-    <Button style={{width:"3rem",height:'3rem'}} variant='outlined-none' >
+    <Button style={{width:"3rem",height:'3rem'}} variant='outline-primary' className='rounded-circle' >
     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
 	  viewBox="0 0 902.86 902.86">
 <g>
@@ -58,6 +58,19 @@ export function Navbar(){
 <g>
 </g>
 </svg>
+<div className='rounded-circle bg-danger d-flex justify-content-center align-items-center'
+style = {{
+	color:'white',
+	width:'1.5rem',
+	height:'1.5rem',
+	position:'absolute',
+	bottom:0,
+	right:0,
+	transform:'translate(25%,25%)'
+}}>
+3
+</div>
+
 
     </Button>
 
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import {Row,Col} from 'react-bootstrap';
+import { StoreItem } from '../components/StoreItem';
+import StoreItems from '../store/items.json';
 export function Store(){
 return (
-        <div>
-            Store
-        </div>
+       <>
+       <h1>Store</h1>
+       <Row md={3} xs={1} lg={2} className="g-3">
+    {StoreItems.map((item)=>(
+        <Col key={item.id}>
+    <StoreItem {...item} />
+        </Col>
+    ))}
+       </Row>
+        
+
+       </>
     )
 }
